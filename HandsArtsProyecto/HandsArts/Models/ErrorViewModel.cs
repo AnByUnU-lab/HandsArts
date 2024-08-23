525183963f47262622871d39bbe483879f28336c
Byron Iturriaga <byron.iturriaga@outlook.com>
2024-08-22 21:54:41 -0400

    Primer commit

---
+++ HandsArtsProyecto/HandsArts/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HandsArts.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
