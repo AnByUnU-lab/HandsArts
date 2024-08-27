c992afd6b70eb282fac1f048eba5d9e919e12be8
Andrea Banda Oyarzún <andreabandao@hotmail.com>
2024-08-26 22:33:48 -0400

    Bloqueo de archivos de configuracion

--- HandsArtsProyecto/HandsArts/obj/Debug/net8.0/HandsArts.AssemblyInfo.cs
+++ HandsArtsProyecto/HandsArts/obj/Debug/net8.0/HandsArts.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HandsArts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6f1287a300ff6d59179362decfbcdb5624691fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd5ab38cfa88140c069e442179a29ff89aa88fe7")]
 [assembly: System.Reflection.AssemblyProductAttribute("HandsArts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HandsArts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
