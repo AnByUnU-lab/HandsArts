fcda97a45506da303fe4c02118e43a09aea13a29
Byron Iturriaga <byron.iturriaga@outlook.com>
2024-08-27 22:52:11 -0400

    Cambios

--- HandsArtsProyecto/HandsArts/obj/Debug/net8.0/HandsArts.AssemblyInfo.cs
+++ HandsArtsProyecto/HandsArts/obj/Debug/net8.0/HandsArts.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HandsArts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd5ab38cfa88140c069e442179a29ff89aa88fe7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c2f644640f945b6b05d17a400e4862805cfbfea")]
 [assembly: System.Reflection.AssemblyProductAttribute("HandsArts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HandsArts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
